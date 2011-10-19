e491cffcc9a80b8c01afe4f25a1c0664ca8b40c7
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-10-18 21:14:55 -0400

    source_root method for generator

--- lib/rails/generators/usda/models/models_generator.rb
+++ lib/rails/generators/usda/models/models_generator.rb
@@ -3,6 +3,10 @@ module Usda
     class ModelsGenerator < Rails::Generators::Base
       desc 'Creates models under the Usda namespace to be loaded with USDA data'
 
+      def self.source_root
+        File.expand_path("../templates", __FILE__)
+      end
+
       def create_usda_models
         template 'food.rb', File.join('app', 'models', 'usda', 'food.rb')
         #todo - other models
